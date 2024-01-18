e39b21722f23f6aed3760a7538847debaa84f4c1
Adhish De <44611777+adhish29@users.noreply.github.com>
2024-01-18 17:17:44 +0530

    dockerized email & enrolment service - V1

--- email-Service/src/emailWorker.ts
+++ email-Service/src/emailWorker.ts
@@ -37,8 +37,8 @@ const emailWorker = new Worker(
     connection: {
       host: process.env.REDIS_HOST,
       port: Number(process.env.REDIS_PORT),
-      username: process.env.REDIS_USERNAME,
-      password: process.env.REDIS_PASSWORD,
+      // username: process.env.REDIS_USERNAME,
+      // password: process.env.REDIS_PASSWORD,
     },
   }
 );
